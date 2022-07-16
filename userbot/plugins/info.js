5a444993a857de8cb0722f1a3b3ce4decab70258
Ber4t Sensei <73439036+Ber4tBey@users.noreply.github.com>
2022-07-16 18:58:58 +0300

    Fix

--- userbot/plugins/info.js
+++ userbot/plugins/info.js
@@ -4,28 +4,32 @@ you may not use this file except in compliance with the License.
 Lavanstax - Ber4tbey
 */
 
-     module.exports.run = async(client, message,args) => {
-        let mesaj = args.join(' ');
-        message.delete();
-        message.chat.sendMessage('Bilgiler alınıyor...').then(() => {
-            client.fetchUser(mesaj).then((user) =>
-            message.chat.sendMessage(
-        `Tam İsim: ${user.fullName}
-        Kullanıcı Adı: ${user.username}
-        ID: ${user.id}
-        Biyografi:\n${user.biography}
-        Takipçi: ${user.followerCount}
-        Takip: ${user.followingCount}
-        Onaylı: ${user.isVerified}
-        Gizli: ${user.isPrivate}
-        Paylaşım: ${user.mediaCount}
+module.exports.run = async(client, message,args) => {
+    message.delete();
+   
+    let mesaj = args.join(' ');
+    if (mesaj.length < 1) return message.reply(`Bilgileri getirebilmem için bir kullanıcı adına ihtiyacım var. Kullanım: .info berathanyedibela`);
+    message.delete();
+    message.chat.sendMessage('Bilgiler alınıyor...').then(() => {
+        client.fetchUser(mesaj).then((user) =>
+    
+        message.chat.sendMessage(
+    `Tam İsim: ${user.fullName}
+    Kullanıcı Adı: ${user.username}
+    ID: ${user.id}
+    Biyografi:\n${user.biography}
+    Takipçi: ${user.followerCount}
+    Takip: ${user.followingCount}
+    Onaylı: ${user.isVerified}
+    Gizli: ${user.isPrivate}
+    Paylaşım: ${user.mediaCount}
 `)
-        )
-        })
+    )
+    }) 
 
-    }
+}
 
 module.exports.config = {
-    command: 'info',
-    description: 'Kişi hakkında bilgi verir.',
+command: 'sinfo',
+description: 'Kişi hakkında bilgi verir.',
 }
