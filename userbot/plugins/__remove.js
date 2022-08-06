8996941ef5f9bd01d16134335461b855d020da38
Ber4t Sensei <73439036+Ber4tBey@users.noreply.github.com>
2022-08-07 00:32:17 +0300

    Update __remove.js

--- userbot/plugins/__remove.js
+++ userbot/plugins/__remove.js
@@ -6,12 +6,16 @@ Lavanstax - Ber4tbey
 
 const fs = require('fs');
 const Db = require('./sql/plugin');
-
+const Config = require('../../config');
 const Language = require('../../language');
 const Lang = Language.getString('_plugin');
+const Heroku = require('heroku-client');
 
+const heroku = new Heroku({
+  token: Config.API_KEY
+});
 
-
+let baseURI = '/apps/' + Config.APP_NAME;
 module.exports.run = async (bot, message, args,match) => {
     let mesaj = args.join(' ');
     if (mesaj === '') return  message.chat.sendMessage(Lang.NEED_PLUGIN);
@@ -23,6 +27,9 @@ module.exports.run = async (bot, message, args,match) => {
         delete require.cache[require.resolve('./' + mesaj + '.js')]
         fs.unlinkSync('./userbot/plugins/' + mesaj + '.js');
         return message.chat.sendMessage( Lang.DELETED);
+        await heroku.delete(baseURI + '/dynos').catch(async (error) => {
+         message.chat.sendMessage(error.message);
+    });
     }
 };
         
