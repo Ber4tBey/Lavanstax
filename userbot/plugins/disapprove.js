be92de3f3fa3e990ec149df85a223622c5cf1a2e
Ber4t Sensei <73439036+Ber4tBey@users.noreply.github.com>
2022-09-03 20:38:07 +0300

    FİX

--- userbot/plugins/disapprove.js
+++ userbot/plugins/disapprove.js
@@ -1,11 +1,4 @@
-
-const Db = require('./sql/pmpermit');
-const Heroku = require('heroku-client');
-
-
-
-
-
+const fs = require("fs")
 
 
 module.exports.run = async (bot, message, args,match) => {
@@ -14,21 +7,28 @@ module.exports.run = async (bot, message, args,match) => {
     ann.each(user => chatt.push(user.username))
    idd = (chatt[0])
  
-    var aprv = await Db.PermitDB.findAll({
-        where: {uid: idd}
-    });
-    
-    if (aprv.length >= 1) {
-        await aprv[0].destroy();
-        await message.chat.sendMessage("Artık mesaj gönderemez")
+   const data = fs.readFileSync(`./${chat_id}.json`,
+   {encoding:'utf8', flag:'r'});
+    pardata = JSON.parse(data)
+
+    if (!pardata.approve) {
+        
+        await message.chat.sendMessage("Zaten mesaj gönderemiyor")
         return false;
     } else {
-        await message.chat.sendMessage("Zaten Mesaj gönderemiyor")
+        let warn1 = { 
+            toplam : 0,
+            
+            
+        };
+        let dataa = JSON.stringify(warn1);
+        fs.writeFileSync(`${idd}.json`, dataa); 
+        await message.chat.sendMessage("Artık mesaj gönderemezsin")
     }
     
 }
 
 module.exports.config = {
     command: "disapprove",
-    description: `Dm engeller`
+    description: `DM engeller`
 }
